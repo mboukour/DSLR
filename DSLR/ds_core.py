7edf0860647aa3c619d47fefb07e83e8ecbed94c
ruined <dynamicamine@gmail.com>
2025-05-17 16:08:39 +0100

    Visual Histogram

--- DSLR/ds_core.py
+++ DSLR/ds_core.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from DSLR.dslr_math import *
 import matplotlib.pyplot as plt
-
+import matplotlib.gridspec as gridspec
 
 def describe(df: pd.DataFrame) -> pd.DataFrame:
     numerical_cols = df.select_dtypes(include=["number"]).columns
@@ -25,27 +25,30 @@ def describe(df: pd.DataFrame) -> pd.DataFrame:
 class Histogram:
     def __init__(self, df: pd.DataFrame):
         df = df.drop(columns=["First Name", "Last Name", "Birthday", "Best Hand"])
-        cph = df.groupby("Hogwarts House")[df.columns[1:]] #group and remove House Name
-        data = []
-        indexes = cph.groups.keys()
-        for name, group in cph:
-            mean_list = []
-            for course in group.columns:
-                mean = calculate_mean(group[course])
-                mean_list.append(mean)
-            data.append(mean_list)
-        course_means_df = pd.DataFrame(data=data, index=indexes, columns=df.columns[1:])
-        stds = {}
-        for course in course_means_df.columns:
-            stds[course] = calculate_std(df[course])
-        stds = dict(sorted(stds.items(), key=lambda x: x[1]))
-        homogeneous_course = next(iter(stds))
-        hc_groups = df.groupby("Hogwarts House")[homogeneous_course]
-        for name, group in hc_groups:
-            plt.hist(group, alpha=0.5, label=name)
-        plt.title(f"Histogram representing {homogeneous_course} scores acorss all houses")
-        plt.xlabel("Scores")
-        plt.legend()
+        courses = df.columns[1:]
+        houses = df.groupby("Hogwarts House")[courses] #group and remove House Name
+        self.__slot = [0,0]
+        self.__cols = 3
+        self.__rows = 5
+        fig = plt.figure(figsize=(8, 10))
+        gs = gridspec.GridSpec(nrows=5, ncols=3, figure=fig)
+        for course in courses:
+            ax = fig.add_subplot(gs[self.__slot[0], self.__slot[1]])
+            ax.set_title(course)
+            for name, house in houses:
+                ax.hist(x=house[course], alpha=0.5)
+            self.__next_slot()
+        plt.tight_layout()
+        
+            
+    def __next_slot(self) -> None:
+        if self.__slot[1] < self.__cols - 1:
+            self.__slot[1] += 1
+        elif self.__slot[0] < self.__rows - 1:
+            self.__slot[0] += 1
+            self.__slot[1] = 0
+
+
 
     def show(self) -> None:
         plt.show()
--- histogram.py
+++ histogram.py
@@ -8,4 +8,3 @@ if __name__  == "__main__":
     df = pd.read_csv("datasets/dataset_train.csv", index_col="Index")
     hist = Histogram(df)
     hist.savefig("images/histogram.png")
-
